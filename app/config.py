913226d07a1d13c0e265af9795b31c09c2b24468
Adam Findley <drfindley@gmail.com>
2010-09-14 03:06:48 -0700

    better comment out the iTunes Folder I'm not using

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ hosts = {
         'base_folder':'/Volumes/data/adam/Dropbox/Dropbox/Music Transfer',
         'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude Cuttooth]', 'iTunes [Exclude Melatonin]'],
         'backup_folders': [],
-        'itunes_folder': '/Volumes/data/iTunes/iTunes Music/Automatically Add to iTunes',
+        #'itunes_folder': '/Volumes/data/iTunes/iTunes Music/Automatically Add to iTunes',
         'itunes_folder': '/Users/adam/Music/iTunes Test/iTunes Media/Automatically Add to iTunes',
     },
     'melatonin':{
