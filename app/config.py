2fa85260b35d8adcbf349d5f794b3eed5790593f
Adam Findley <drfindley@gmail.com>
2011-01-18 02:12:47 -0800

    Added cuttooth back in now that it no longer sucks at iTunes

--- app/config.py
+++ app/config.py
@@ -1,21 +1,21 @@
 # CONFIG
 
 hosts = {
-#    'cuttooth':{
-#        'base_folder':'/Volumes/data/adam/Dropbox/Music Transfer',
-#        'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude Fasttrack]', 'iTunes [Exclude Melatonin]'],
-#        'backup_folders': [],
-#        'itunes_folder': '',
+    'cuttooth':{
+        'base_folder':'/Users/adam/Dropbox/Music Transfer',
+        'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude cuttooth]', 'iTunes [Exclude fasttrack]', 'iTunes [Exclude melatonin]'],
+        'backup_folders': [],
+        'itunes_folder': '/Users/adam/Music/iTunes/iTunes Media/Automatically Add to iTunes',
 #    },
     'fasttrack':{
-        'base_folder':'/Volumes/data/adam/Dropbox/Dropbox/Music Transfer',
-        'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude Cuttooth]', 'iTunes [Exclude Melatonin]'],
+        'base_folder':'/Users/adam/Dropbox/Music Transfer',
+        'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude cuttooth]', 'iTunes [Exclude fasttrack]', 'iTunes [Exclude melatonin]'],
         'backup_folders': [],
-        'itunes_folder': '/Volumes/data/iTunes/iTunes Music/Automatically Add to iTunes',
+        'itunes_folder': '/Users/adam/Music/iTunes/iTunes Music/Automatically Add to iTunes',
     },
     'melatonin':{
         'base_folder':'/Users/adam/Dropbox/Music Transfer',
-        'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude Cuttooth]', 'iTunes [Exclude Fasttrack]'],
+        'monitor_folders': ['Amazon MP3', 'Emusic', 'other', 'iTunes [Exclude cuttooth]', 'iTunes [Exclude fasttrack]', 'iTunes [Exclude melatonin]'],
         'backup_folders': ['/Volumes/Drobo/Music [Lossy]'],
         'itunes_folder': '/Users/adam/Music/iTunes/iTunes Media/Automatically Add to iTunes',
     },
